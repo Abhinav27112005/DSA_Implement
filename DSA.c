d3269e08fa3c4fc8748b1199bbc001b36a452812
Abhianv Kumar Jha <abhinavkumarjha90@gmail.com>
2022-05-24 08:45:37 +0530

    ignoring exe and object file

--- DSA.c
+++ DSA.c
@@ -118,7 +118,7 @@ void dele_end()
     }
 }
 
-Node *insert_spe(Node *Start, int item) // Inserting a node in the list at specific positon
+Node *insert_spe(Node *start, int item) // Inserting a node in the list at specific positon
 {
     Node *new_node, *p, *loc;
     int temp;
@@ -127,11 +127,105 @@ Node *insert_spe(Node *Start, int item) // Inserting a node in the list at speci
     scanf("%d", &temp);
     new_node = (Node *)malloc(sizeof(Node));
     new_node->info = item;
-    p = Start;
-    loc = Start;
+    p = start;
+    loc = start;
     while (p->info != temp)
     {
         /* code */
         p = p->next;
+    }
+    p = p->next;
+    loc = p->next;
+    p->next = new_node;
+    new_node->next = loc;
+    return start;
+}
+
+Node *Delete_spe(Node *start, int item)
+{
+    int temp;
+    Node *ptr;
+    if (start == NULL)
+    {
+        /* code */
+        printf("list is Empty");
+    }
+    printf("\nEnter a Number You want to Delete!!");
+    scanf("%d", &temp);
+    ptr = start;
+
+    while (ptr->info != temp)
+    {
+        /* code */
+        ptr = ptr->next;
+    }
+    printf("\nNumber You Have Deleted is : %d", ptr->info);
+    free(ptr);
+    return start;
+}
+
+int main(int argc, char const *argv[])
+{
+    int choice, num;
+    char ch;
+    system("cls");
+    createemptylist(Start);
+    do
+    {
+        /* code */
+        printf("\n1. Insert Element at Beginning!");
+        printf("\n2. Insert Element at End Position!!");
+        printf("\n3. Insert Element at Specific Position!");
+        printf("\n4. Traverse the List");
+        printf("\n5. Delete Element at Beginning!");
+        printf("\n6. Delete Element at Last!");
+        printf("\n7. Delete Element at Specific Position!");
+        printf("\n8. Exit");
+        printf("\n\n\nEnter Your Choice!!");
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+        case 1:
+            /* code */
+            printf("\nEnter a Number You Want to Enter: ");
+            scanf("%d", &num);
+            insertbegin(num);
+            break;
+        case 2:
+            printf("\nEnter a Number You Want to Enter: ");
+            scanf("%d", &num);
+            insert_at_end(num);
+            break;
+        case 3:
+            printf("\nEnter a Number You Want to Enter: ");
+            scanf("%d", &num);
+            Start = insert_spe(Start, num);
+            break;
+        case 4:
+            printf("\nTraversing a List: ");
+            traverse(Start);
+            break;
+        case 5:
+            printf("\nDeleting the Number!");
+            dele_beg();
+            break;
+        case 6:
+            printf("\nDeleting the Number!!");
+            dele_end();
+            break;
+        case 7:
+            printf("\nEnter a Number You Want to Delete: ");
+            scanf("%d", &num);
+            Start = Delete_spe(Start, num);
+            break;
+        case 8:
+            exit(0);
+        default:
+            printf("\nEnter a Valid Choice!!");
+            break;
         }
+    } while (choice);
+
+    return 0;
 }
